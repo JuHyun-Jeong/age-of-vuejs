ab5945b59027d45a47236213f475bb9219fa7e9b
정주현 <wjdeoaks92@gmail.com>
2021-08-02 20:07:17 +0900

    Age of Vue.js 수강 섹션 10 : 프로젝트 생성 도구 - Vue CLI 섹션 11 : 싱글 파일 컴포넌트 섹션 12 : 최종 프로젝트 - 사용자 입력 폼 만들기

---
+++ learn-vue-js/vue-cli/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <app-header v-bind:propsdata="str"
+                v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader";
+
+export default {
+  data() {
+    return {
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr(){
+      this.str = 'hi'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
